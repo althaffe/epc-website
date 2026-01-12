60f24f6a02d06e343bab9fd97b6944480cdc2a93
Muhammad Althaf <althaf.mec@gmail.com>
2026-01-12 22:49:31 +0530

    add content

--- tina/collections/page.ts
+++ tina/collections/page.ts
@@ -1,4 +1,77 @@
-import type { Collection } from "tinacms";
+import type { Collection, Template } from "tinacms";
+
+const richTextBlock: Template = {
+  name: "richText",
+  label: "Rich Text",
+  fields: [
+    {
+      type: "rich-text",
+      name: "body",
+      label: "Body",
+    },
+  ],
+};
+
+const imageBlock: Template = {
+  name: "image",
+  label: "Image",
+  fields: [
+    {
+      type: "image",
+      name: "image",
+      label: "Image",
+    },
+    {
+      type: "string",
+      name: "caption",
+      label: "Caption",
+    },
+  ],
+};
+
+const embedBlock: Template = {
+  name: "embed",
+  label: "Youtube Embed",
+  fields: [
+    {
+      type: "string",
+      name: "youtubeId",
+      label: "Youtube Video ID",
+    },
+  ],
+};
+
+const gridBlock: Template = {
+  name: "grid",
+  label: "Grid",
+  ui: {
+    defaultItem: () => {
+      return {
+        columnCount: "2",
+        columns: [],
+      };
+    },
+  },
+  fields: [
+    {
+      name: "columnCount",
+      label: "Number of columns",
+      type: "string",
+      options: [
+        { value: "2", label: "2" },
+        { value: "3", label: "3" },
+        { value: "4", label: "4" },
+      ],
+    },
+    {
+      name: "columns",
+      label: "Columns",
+      type: "object",
+      list: true,
+      templates: [richTextBlock, imageBlock],
+    },
+  ],
+};
 
 export const PageCollection: Collection = {
   name: "page",
@@ -14,13 +87,46 @@ export const PageCollection: Collection = {
     {
       name: "seoTitle",
       type: "string",
-      required: true
+      required: true,
     },
     {
-      name: "body",
-      type: "rich-text",
-      isBody: true,
-      required: true
-    }
-  ]
-}
+      name: "banner",
+      label: "Banner",
+      type: "object",
+      list: true,
+      fields: [
+        {
+          type: "string",
+          name: "headline",
+          label: "Headline",
+        },
+        {
+          type: "string",
+          name: "content",
+          label: "Content",
+          ui: {
+            component: "textarea",
+          },
+        },
+        {
+          type: "image",
+          name: "image",
+          label: "Image",
+        },
+      ],
+    },
+    {
+      name: "content",
+      type: "object",
+      list: true,
+      label: "Content",
+      templates: [richTextBlock, imageBlock, embedBlock, gridBlock],
+    },
+    // {
+    //   name: "body",
+    //   type: "rich-text",
+    //   isBody: true,
+    //   required: true,
+    // },
+  ],
+};
--- tina/pages/Page.tsx
+++ tina/pages/Page.tsx
@@ -1,29 +1,43 @@
 import { tinaField, useTina } from "tinacms/dist/react";
 import type { PageQuery, PageQueryVariables } from "../__generated__/types";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
+import Banner from "../../src/components/react/Banner";
+import Blocks from "../../src/components/react/Blocks";
 
 type Props = {
-	variables: PageQueryVariables;
-	data: PageQuery;
-	query: string;
-}
+  variables: PageQueryVariables;
+  data: PageQuery;
+  query: string;
+};
 
 const TinaPage = (props: Props) => {
-	const { data } = useTina({
-		query: props.query,
-		variables: props.variables,
-		data: props.data,
-	})
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
 
-	const page = data.page;
+  const page = data.page;
 
-	return (
-		<main>
-			<div data-tina-field={tinaField(page, "body")}>
-				<TinaMarkdown content={page.body} />
-			</div>
-		</main>
-	)
-}
+  console.log(page);
+
+  return (
+    <>
+      <Banner page={page} />
+
+      <div className="py-6 bg-[#045472]">
+        <div className="container uppercase text-white font-medium text-2xl">
+          {page.seoTitle}
+        </div>
+      </div>
+      <main className="container">
+        <Blocks page={page} />
+        {/*<div className="prose py-8" data-tina-field={tinaField(page, "body")}>
+          <TinaMarkdown content={page.body} />
+        </div>*/}
+      </main>
+    </>
+  );
+};
 
 export default TinaPage;
