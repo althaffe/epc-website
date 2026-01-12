50da5d6ab310d3f4db79643c224aa3b377c58bc8
Muhammad Althaf <althaf.mec@gmail.com>
2026-01-12 23:33:13 +0530

    fix imports

--- tina/pages/Page.tsx
+++ tina/pages/Page.tsx
@@ -1,8 +1,8 @@
 import { tinaField, useTina } from "tinacms/dist/react";
 import type { PageQuery, PageQueryVariables } from "../__generated__/types";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
-import Banner from "../../src/components/react/Banner.tsx";
-import Blocks from "../../src/components/react/Blocks.tsx";
+import Banner from "../components/Banner.tsx";
+import Blocks from "../components/Blocks.tsx";
 
 type Props = {
   variables: PageQueryVariables;
